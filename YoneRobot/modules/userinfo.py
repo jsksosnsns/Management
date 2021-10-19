7a2ccbbf085b978bc0f77b3a9e1e54ab0de6db19
Aarukami <91074890+Aarukami@users.noreply.github.com>
2021-10-19 13:59:54 +0530

    Update userinfo.py

--- YoneRobot/modules/userinfo.py
+++ YoneRobot/modules/userinfo.py
@@ -306,7 +306,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/DeZilleius/100">?</a>]'.format(
+        text += ' [<a href="https://t.me/mikasa_update_channel/3">?</a>]'.format(
             bot.username
         )
 
